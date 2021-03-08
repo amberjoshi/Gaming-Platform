7d73aa7ae33295d7434d9a032ee564b62e5191fe
amberjoshi <66717335+amberjoshi@users.noreply.github.com>
2021-03-08 15:15:11 +0530

    Add files via upload

---
+++ serverUtility/Server.java
@@ -0,0 +1,14 @@
+package serverUtility;
+public class Server
+{
+public int getBalance(int id)
+{
+/*Code for establishing connection with database and
+getting current balace for particular id.*/
+return 15; //for testing purposes
+}
+public void setBalance(int id,int balance)
+{
+//Code for changing the balance in the database
+}
+}
